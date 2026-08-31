7185b63e7151e132893cff2aa279f8db72afa4c1
asa-kdg <kudoasato0607@gmail.com>
2026-08-31 13:10:13 +0900

    adminの追加

--- src/rentacarcli/Admin.java
+++ src/rentacarcli/Admin.java
@@ -1,5 +1,34 @@
 package rentacarcli;
 
 public class Admin {
+	private String Id = "admin";
+	private int pass = 1111;
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		this.Id = id;
+	}
+
+	public int getPass() {
+		return pass;
+	}
+
+	public void setPass(int pass) {
+		this.pass = pass;
+	}
+
+	public boolean certificationPass(String id, int pass) {
+		if (id == getId() && pass == getPass()) {
+			System.out.println("パスワードが認証されました。");
+			return true;
+		} else {
+			System.out.println("IDまたはパスワードが違います。");
+			return false;
+		}
+
+	}
 
 }
