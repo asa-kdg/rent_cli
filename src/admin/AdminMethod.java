4dfd43c94cf108dab3ba574db79cffbf8bc211b9
asa-kdg <kudoasato0607@gmail.com>
2026-09-12 13:35:46 +0900

    あああ

--- src/admin/AdminMethod.java
+++ src/admin/AdminMethod.java
@@ -2,6 +2,8 @@ package admin;
 
 import java.util.Scanner;
 
+import model.ShopManagers;
+
 public class AdminMethod {
 	static Scanner scan = new Scanner(System.in);
 
@@ -32,6 +34,35 @@ public class AdminMethod {
 			System.out.println("パスワードが違います。");
 			return false;
 		}
+	}
+
+	public void selectCommand() {
+		System.out.println("どの操作をしますか");
+		scan.nextLine();
+		System.out.println("1:店舗情報の更新");
+		System.out.println("2:店舗の登録");
+		System.out.println("3:車両の登録");
+		System.out.println("4:車両の削除");
+		System.out.println("5:予約一覧の表示");
+		System.out.println("6:予約状態の変更");
+		System.out.println("7:");
+		int selctCommand = scan.nextInt();
+		if (selctCommand == 1) {
+			ShopManagers.updateShop();
+		} else if (selctCommand == 2) {
+			ShopManagers.addShop();
+		} else if (selctCommand == 3) {
+			ShopManagers.addShop();
+		} else if (selctCommand == 4) {
+			ShopManagers.addShop();
+		} else if (selctCommand == 5) {
+			ShopManagers.addShop();
+		} else if (selctCommand == 6) {
+			ShopManagers.addShop();
+		} else if (selctCommand == 7) {
+			ShopManagers.addShop();
+		}
+
 	}
 
 }
