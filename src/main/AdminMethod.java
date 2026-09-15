8228e63ea4dfa27d5fc25eddda1462f02def1b63
asa-kdg <kudoasato0607@gmail.com>
2026-09-15 22:33:28 +0900

    ちょいとだけ

--- src/main/AdminMethod.java
+++ src/main/AdminMethod.java
@@ -7,21 +7,35 @@ import model.ShopManagers;
 public class AdminMethod {
 	static Scanner scan = new Scanner(System.in);
 
-	public static int selectFunc() {
+	public static int selectAdminFunction() {
+		System.out.println("どの操作をしますか");
+		scan.nextLine();
 		while (true) {
-			System.out.println("1:");
-			System.out.println("2:");
-			System.out.println("3:");
-			System.out.println("4:");
-			System.out.println("5:");
-			int select = scan.nextInt();
+			System.out.println("1:車両更新");
+			System.out.println("2:車両登録");
+			System.out.println("3:車両削除");
+			System.out.println("4:予約確認");
+			System.out.println("5:予約状態変更");
+			int selectFunc = scan.nextInt();
 
-			if (select <= 5 && select >= 1) {
-				return select;
+			if (selectFunc <= 5 && selectFunc >= 1) {
+				return selectFunc;
 			} else {
 				System.out.println("不正な入力です。数字で選択してください");
 			}
+			if (selectFunc == 1) {
+				ShopManagers.updateShop();
+			} else if (selectFunc == 2) {
+				ShopManagers.addShop();
+			} else if (selectFunc == 3) {
+				ShopManagers.addShop();
+			} else if (selectFunc == 4) {
+				ShopManagers.addShop();
+			} else if (selectFunc == 5) {
+				ShopManagers.addShop();
+			}
 		}
+
 	}
 
 	public static boolean certificationPass() {
@@ -34,35 +48,6 @@ public class AdminMethod {
 			System.out.println("パスワードが違います。");
 			return false;
 		}
-	}
-
-	public void selectCommand() {
-		System.out.println("どの操作をしますか");
-		scan.nextLine();
-		System.out.println("1:店舗情報の更新");
-		System.out.println("2:店舗の登録");
-		System.out.println("3:車両の登録");
-		System.out.println("4:車両の削除");
-		System.out.println("5:予約一覧の表示");
-		System.out.println("6:予約状態の変更");
-		System.out.println("7:");
-		int selctCommand = scan.nextInt();
-		if (selctCommand == 1) {
-			ShopManagers.updateShop();
-		} else if (selctCommand == 2) {
-			ShopManagers.addShop();
-		} else if (selctCommand == 3) {
-			ShopManagers.addShop();
-		} else if (selctCommand == 4) {
-			ShopManagers.addShop();
-		} else if (selctCommand == 5) {
-			ShopManagers.addShop();
-		} else if (selctCommand == 6) {
-			ShopManagers.addShop();
-		} else if (selctCommand == 7) {
-			ShopManagers.addShop();
-		}
-
 	}
 
 }
--- src/main/UserMethod.java
+++ src/main/UserMethod.java
@@ -1,5 +1,41 @@
 package main;
 
+import java.util.Scanner;
+
+import model.ShopManagers;
+
 public class UserMethod {
+	static Scanner scan = new Scanner(System.in);
+
+	public static int selectUserFunction() {
+		System.out.println("どの操作をしますか");
+		scan.nextLine();
+		while (true) {
+			System.out.println("1:店舗一覧表示");
+			System.out.println("2:車種一覧表示");
+			System.out.println("3:予約");
+			System.out.println("4:予約確認");
+			System.out.println("5:予約変更、キャンセル");
+			int selectFunc = scan.nextInt();
+
+			if (selectFunc <= 5 && selectFunc >= 1) {
+				return selectFunc;
+			} else {
+				System.out.println("不正な入力です。数字で選択してください");
+			}
+
+			if (selectFunc == 1) {
+				ShopManagers.showShop();
+			} else if (selectFunc == 2) {
+				ShopManagers.addShop();
+			} else if (selectFunc == 3) {
+				ShopManagers.addShop();
+			} else if (selectFunc == 4) {
+				ShopManagers.addShop();
+			} else if (selectFunc == 5) {
+				ShopManagers.addShop();
+			}
+		}
+	}
 
 }
